8de6d218d9f0b62eb7f817ec97a2df1ce8526743
avanza-sano <sano-takahiro@avnz.co.jp>
2016-04-07 15:34:12 +0900

    rename_key.rbには処理を入れていないので不要なテストを削除する

--- spec/rename_key_spec.rb
+++ spec/rename_key_spec.rb
@@ -4,8 +4,4 @@ describe RenameKey do
   it 'has a version number' do
     expect(RenameKey::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
