599d04868e7280d3232a8e96629ab26eb9bd0219
avanza-sano <sano-takahiro@avnz.co.jp>
2016-04-07 15:42:21 +0900

    ハッシュのキー値を入れ替える実装完了

--- lib/rename_key/ext/hash.rb
+++ lib/rename_key/ext/hash.rb
@@ -1,5 +1,12 @@
 class Hash
-  def rename_key(keys)
-    keys
+  def rename_key(rename_keys)
+    return_hash = {}
+    self.each do |self_key, self_value|
+      if rename_keys.has_key?(self_key)
+        self_key = rename_keys[self_key]
+      end
+      return_hash[self_key] = self_value
+    end
+    return_hash
   end
 end
