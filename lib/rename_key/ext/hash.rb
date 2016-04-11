0e1dd052ffb23bef4cfbb59f012f37a1416259e0
onigra <3280467rec@gmail.com>
2016-04-11 15:45:52 +0900

    引数がHashじゃなかったらTypeErrorを発生させるようにした

--- lib/rename_key/ext/hash.rb
+++ lib/rename_key/ext/hash.rb
@@ -1,5 +1,7 @@
 class Hash
   def rename_key(rename_keys = {})
+    raise TypeError, "Type of argument must be hash." unless rename_keys.instance_of?(Hash)
+
     self.reduce({}) do |res, (key, val)|
       if rename_keys.include?(key)
         res[rename_keys[key]] = val
--- spec/ext/hash_spec.rb
+++ spec/ext/hash_spec.rb
@@ -2,12 +2,37 @@ describe Hash do
   [
     [ { foo: "foo", bar: "bar", baz: "baz" }, { foo: :Foo, bar: :Bar }, { Foo: "foo", Bar: "bar", baz: "baz" } ],
     [ { foo: "foo", bar: "bar", baz: "baz" }, { "foo" => "foo", "bar" => "bar" }, { foo: "foo", bar: "bar", baz: "baz" } ],
+    [ { foo: "foo", bar: "bar", baz: "baz" }, {}, { foo: "foo", bar: "bar", baz: "baz" } ],
   ].each do |base, arg, expect_value|
     describe "#rename_key" do
       context "#{base}.rename_key #{arg}" do
         subject { base.rename_key arg }
         it { is_expected.to eq expect_value }
       end
+    end
+  end
+
+  context "Hash以外の引数" do
+    let(:base) { { foo: "foo", bar: "bar", baz: "baz" } }
+
+    context "String" do
+      let(:arg) { "Foo" }
+      it { expect { base.rename_key arg }.to raise_error TypeError }
+    end
+
+    context "Integer" do
+      let(:arg) { 1 }
+      it { expect { base.rename_key arg }.to raise_error TypeError }
+    end
+
+    context "Array" do
+      let(:arg) { ["foo", "bar", "baz"] }
+      it { expect { base.rename_key arg }.to raise_error TypeError }
+    end
+
+    context "NilClass" do
+      let(:arg) { nil }
+      it { expect { base.rename_key arg }.to raise_error TypeError }
     end
   end
 end
